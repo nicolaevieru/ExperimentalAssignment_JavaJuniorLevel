ecced16e20d825c19d801c50b4c0b06df4f9f2cc
Ionesi Cristian <cristian.ionesi@fortech.ro>
2017-04-21 14:06:22 +0300

    First Commit

---
+++ src/main/java/com/fortech/VinylStoreApplication.java
@@ -0,0 +1,15 @@
+package com.fortech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylStoreApplication.class, args);
+		
+		System.out.println("yiyiy");
+		System.out.println("fsgfasd");
+	}
+}
