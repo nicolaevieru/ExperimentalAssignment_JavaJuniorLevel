41b64ec5618d6008a84cf89bb878456e373768ff
Ionesi Cristian <cristian.ionesi@fortech.ro>
2017-04-27 09:18:24 +0300

    Added /api/vinyls endpoint and configure project to work with swagger

--- src/main/java/com/fortech/VinylStoreApplication.java
+++ src/main/java/com/fortech/VinylStoreApplication.java
@@ -2,9 +2,12 @@ package com.fortech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
-public class VinylStoreApplication {
+public class VinylStoreApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VinylStoreApplication.class, args);
