02e013778438319850f03a5c664b0871d42007d7
Ionesi Cristian <cristian.ionesi@fortech.ro>
2017-05-08 12:33:14 +0300

    Improve test classes

--- src/test/java/com/fortech/controller/TestTest.java
+++ src/test/java/com/fortech/controller/TestTest.java
@@ -1,7 +1,13 @@
 package com.fortech.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +16,12 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.fortech.model.Vinyl;
 import com.fortech.repository.VinylRepository;
+import com.fortech.service.VinylService;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -24,9 +33,24 @@ public class TestTest {
 	@Autowired
 	private TestRestTemplate restTemplate;
 	
+	@Mock
+	private VinylService vinylService;
+	
+	@InjectMocks
+	private VinylController vinylController;
+	
+	private MockMvc mockMvc;
+	
 	@Autowired
 	VinylRepository vinylRepo;
 
+	@org.junit.Before
+	public void setup(){
+		MockitoAnnotations.initMocks(this);
+		
+		this.mockMvc = MockMvcBuilders.standaloneSetup(vinylController).build();
+	}
+	
 	@Test
 	public void test() {
 		Vinyl vinyl = new Vinyl();
@@ -34,6 +58,18 @@ public class TestTest {
 		vinyl.setName("bbbb");
 		vinyl.setStock(100);
 		vinylRepo.save(vinyl);
+	}
+	
+	@Test
+	public void testMockito() throws Exception{
+		
+		
+		this.mockMvc.perform(post("/api/vinyls")
+					.param("token", "513879925")
+					.param("name","ion")
+					.param("cost", "12")
+					.param("stock", "100"))
+					.andExpect(status().isCreated());
 	}
 
 }
