5f9fb3fad3a6ee927e78b057e5c62196c69241b9
Dragos Slujeru <dragos.slujeru@fortech.ro>
2017-05-11 13:48:57 +0300

    added @DirtiesContext to test class

--- src/test/java/com/fortech/controller/AbstractTest.java
+++ src/test/java/com/fortech/controller/AbstractTest.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -21,7 +21,7 @@ import io.restassured.specification.RequestSpecification;
 @RunWith(SpringRunner.class)
 @TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest(webEnvironment = WebEnvironment.DEFINED_PORT)
-@ActiveProfiles("test")
+@DirtiesContext
 @AutoConfigureTestDatabase
 public abstract class AbstractTest {
 	protected static final int PORT = 9000;
